207e6e01e0d79d3625e0f09d37538429fda907a7
Jin Kang <kjinn62@gmail.com>
2020-11-16 16:19:35 -0800

    added requirements

--- getgud.py
+++ getgud.py
@@ -14,29 +14,29 @@ window = pygame.display.set_mode([displayInfo.current_w, displayInfo.current_h])
 running = True
 while running:
 
-        #if user clicks window close, exits program
+        #if user clicks window close or presses the escape key, exits program
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and pygame.K_ESCAPE:
                 running = False
         
         #sets background color to be white
-        window.fill((255,255,255))
+        window.fill((200,200,200))
 
         #sets random x (within 720 pixels) coordinate for circle location
-        randX = random.randint(280, windowWidth- 280)
+        randX = random.randint(280, displayInfo.current_w- 280)
 
         #sets random y (within 480 pixels) coordinate for circle location
-        randY = random.randint(120, windowHeight - 120)
+        randY = random.randint(120, displayInfo.current_h - 120)
         
         #sets random radius
         randR = random.randint(25, 75)
 
         #draws our target on window
-        pygame.draw.circle(window, (255, 0, 0), (windowWidth - randX, windowHeight - randY), randR)
+        pygame.draw.circle(window, (0, 168, 0), (displayInfo.current_w - randX, displayInfo.current_h - randY), randR)
 
         #updates display on window
         pygame.display.update()
-        pygame.time.wait(1000)
+        pygame.time.wait(750)
 
 pygame.quit()
 
